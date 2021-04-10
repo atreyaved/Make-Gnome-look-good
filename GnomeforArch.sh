6e3f1297f0f7b0e898a77603a91d2dfd2d2f37ec
atreyaved <70472507+atreyaved@users.noreply.github.com>
2021-04-10 17:06:09 +0530

    Update GnomeforArch.sh

--- GnomeforArch.sh
+++ GnomeforArch.sh
@@ -5,6 +5,7 @@ sudo pacman -S unzip
 sudo pacman -S wget
 
 
+
 git clone https://github.com/atreyaved/MakingGnomeLookGoodFiles.git
 cd MakingGnomeLookGoodFiles
 
@@ -16,10 +17,14 @@ cp -r material_cursors ~/.icons
 cp -r Canta ~/.local/share/icons
 unzip clipboard-indicatortudmotu.com.v37.shell-extension.zip
 unzip dash-to-dockmicxgx.gmail.com.v69.shell-extension.zip
+unzip user-themegnome-shell-extensions.gcampax.github.com.v42.shell-extension.zip
 mkdir -p ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
 cp -r clipboard-indicatortudmotu.com.v37.shell-extension ~/.local/share/gnome-shell/extensions/clipboard-indicator@tudmotu.com
 mkdir -p ~/.local/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
 cp -r dash-to-dockmicxgx.gmail.com.v69.shell-extension ~/.local/share/gnome-shell/extensions/dash-to-dockmicxgx.gmail.com.v69.shell-extension
+mkdir-p ~/.local/share/gnome-shell/extensions/user-theme@gnome-shell-extensions.gcampax.github.com
+cp -r user-themegnome-shell-extensions.gcampax.github.com.v42.shell-extension ~/.local/share/gnome-shell/extensions/user-theme@gnome-shell-extensions.gcampax.github.com
+
 
 
 gsettings set org.gnome.shell.extensions.user-theme name "Orchis-dark"
@@ -28,6 +33,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "Orchis-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "material_cursors"
 gnome-shell-extension-tool -e clipboard-indicatortudmotu.com.v37.shell-extension
 gnome-shell-extension-tool -e dash-to-dockmicxgx.gmail.com.v69.shell-extension
+gnome-shell-extension-tool -e user-theme@gnome-shell-extensions.gcampax.github.com
 
 cd ..
 rm -rf MakingGnomeLookGoodFiles
