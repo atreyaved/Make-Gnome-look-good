558aec8bcbd64ab9af34b01f60c908c9ed3fa9d1
atreyaved <70472507+atreyaved@users.noreply.github.com>
2021-04-10 16:21:37 +0530

    Add FIles
    Make gnome look good with a single script

---
+++ GnomeForDebian.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt install gnome-shell-extensions
+
+git clone https://github.com/atreyaved/MakingGnomeLookGoodFiles.git
+cd MakingGnomeLookGoodFiles
+
+unzip -q '*.zip'
+cp -r Orchis-dark ~/.themes
+cp -r material_cursors ~/.icons
+cp -r Canta ~/.local/share/icons
+
+gsettings set org.gnome.shell.extensions.user-theme name "Orchis-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Canta"
+gsettings set org.gnome.desktop.interface gtk-theme "Orchis-dark"
+gsettings set org.gnome.desktop.interface cursor-theme "material_cursors"
+
+cd ..
+rm -rf MakingGnomeLookGoodFiles
---
+++ GnomeForFedora.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo dnf install gnome-shell-extension-user-theme
+
+git clone https://github.com/atreyaved/MakingGnomeLookGoodFiles.git
+cd MakingGnomeLookGoodFiles
+
+unzip -q '*.zip'
+cp -r Orchis-dark ~/.themes
+cp -r material_cursors ~/.icons
+cp -r Canta ~/.local/share/icons
+
+gsettings set org.gnome.shell.extensions.user-theme name "Orchis-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Canta"
+gsettings set org.gnome.desktop.interface gtk-theme "Orchis-dark"
+gsettings set org.gnome.desktop.interface cursor-theme "material_cursors"
+
+cd ..
+rm -rf MakingGnomeLookGoodFiles
